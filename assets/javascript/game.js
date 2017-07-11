ebb6490e335bc4ca6a14518716480b62ca2d10df
Sandro Wong <saloiofun@gmail.com>
2017-07-10 23:40:16 -0700

    cleaned the code and added comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,286 +1,296 @@
 $(document).ready(function() {
 
-// Street Fighter Characters
-var fighters = {
+	// Street Fighter Characters
+	var fighters = {
 
-	ryu: {
-		nickname: "Ryu",
-		value: "ryu",
-		health: 1000,
-		attack: 30,
-		counterAttack: 45,
-		selected: false,
-		active: false,
-		image: "ryu.png"
-	},
+		ryu: {
+			nickname: "Ryu",
+			value: "ryu",
+			health: 1000,
+			attack: 30,
+			counterAttack: 45,
+			selected: false,
+			active: false,
+			image: "ryu.png"
+		},
 
-	ken: {
-		nickname: "Ken",
-		value: "ken",
-		health: 1000,
-		attack: 25,
-		counterAttack: 45,
-		selected: false,
-		active: false,
-		image: "ken.png"
-	},
+		ken: {
+			nickname: "Ken",
+			value: "ken",
+			health: 1000,
+			attack: 25,
+			counterAttack: 45,
+			selected: false,
+			active: false,
+			image: "ken.png"
+		},
 
-	akuma: {
-		nickname: "Akuma",
-		value: "akuma",
-		health: 900,
-		attack: 30,
-		counterAttack: 45,
-		selected: false,
-		active: false,
-		image: "akuma.png"
-	},
+		akuma: {
+			nickname: "Akuma",
+			value: "akuma",
+			health: 900,
+			attack: 30,
+			counterAttack: 45,
+			selected: false,
+			active: false,
+			image: "akuma.png"
+		},
 
-	dan: {
-		nickname: "Dan",
-		value: "dan",
-		health: 1000,
-		attack: 20,
-		counterAttack: 35,
-		selected: false,
-		active: false,
-		image: "dan.png"
-	},
+		dan: {
+			nickname: "Dan",
+			value: "dan",
+			health: 1000,
+			attack: 20,
+			counterAttack: 35,
+			selected: false,
+			active: false,
+			image: "dan.png"
+		},
 
-	dhalsim: {
-		nickname: "Dhalsim",
-		value: "dhalsim",
-		health: 900,
-		attack: 25,
-		counterAttack: 40,
-		selected: false,
-		active: false,
-		image: "dhalsim.png"
-	},
+		dhalsim: {
+			nickname: "Dhalsim",
+			value: "dhalsim",
+			health: 900,
+			attack: 25,
+			counterAttack: 40,
+			selected: false,
+			active: false,
+			image: "dhalsim.png"
+		},
 
-	chunLi: {
-		nickname: "Chun-Li",
-		value: "chunLi",
-		health: 950,
-		attack: 35,
-		counterAttack: 35,
-		selected: false,
-		active: false,
-		image: "chunLi.png"
-	},
+		chunLi: {
+			nickname: "Chun-Li",
+			value: "chunLi",
+			health: 950,
+			attack: 35,
+			counterAttack: 35,
+			selected: false,
+			active: false,
+			image: "chunLi.png"
+		},
 
-	sakura: {
-		nickname: "Sakura",
-		value: "sakura",
-		health: 950,
-		attack: 20,
-		counterAttack: 30,
-		selected: false,
-		active: false,
-		image: "sakura.png"
-	},
+		sakura: {
+			nickname: "Sakura",
+			value: "sakura",
+			health: 950,
+			attack: 20,
+			counterAttack: 30,
+			selected: false,
+			active: false,
+			image: "sakura.png"
+		},
 
-	guile: {
-		nickname: "Guile",
-		value: "guile",
-		health: 950,
-		attack: 30,
-		counterAttack: 40,
-		selected: false,
-		active: false,
-		image: "guile.png"
-	},
+		guile: {
+			nickname: "Guile",
+			value: "guile",
+			health: 950,
+			attack: 30,
+			counterAttack: 40,
+			selected: false,
+			active: false,
+			image: "guile.png"
+		},
 
-	elFuerte: {
-		nickname: "El Fuerte",
-		value: "elFuerte",
-		health: 900,
-		attack: 30,
-		counterAttack: 35,
-		selected: false,
-		active: false,
-		image: "elFuerte.png"
-	},
+		elFuerte: {
+			nickname: "El Fuerte",
+			value: "elFuerte",
+			health: 900,
+			attack: 30,
+			counterAttack: 35,
+			selected: false,
+			active: false,
+			image: "elFuerte.png"
+		},
 
-	feiLong: {
-		nickname: "Fei Long",
-		value: "feiLong",
-		health: 950,
-		attack: 25,
-		counterAttack: 40,
-		selected: false,
-		active: false,
-		image: "feiLong.png"
-	}
-};
+		feiLong: {
+			nickname: "Fei Long",
+			value: "feiLong",
+			health: 950,
+			attack: 25,
+			counterAttack: 40,
+			selected: false,
+			active: false,
+			image: "feiLong.png"
+		}
+	};
 
-var fightersArray = [];
-var imagesPath = "assets/images/";
-var player;
-var challenger;
-var defaultAttackPoint;
-var isPlayerSelected = false;
-var isChallengerSelected = false;
-$("#attack").attr("disabled","disabled");
+	//declare variables
+	var fightersArray = [];
+	var imagesPath = "assets/images/";
+	var player;
+	var challenger;
+	var defaultAttackPoint;
+	var isPlayerSelected = false;
+	var isChallengerSelected = false;
 
-//push fighters objects into fightersArray
-$.each(fighters, function(key) {
-	fightersArray.push(key);
-});
+	$("#attack").attr("disabled","disabled");
 
-var populateFightersRow = function(row, numFrom, numTo) {
-	for (var i = numFrom; i < numTo; i++) {	
-		var fighterContainer = $("<div>");		
-		fighterContainer.addClass("col-1 fighterSelect");
-		fighterContainer.attr("value", fighters[fightersArray[i]].value);
+	//push fighters objects into fightersArray
+	$.each(fighters, function(key) {
+		fightersArray.push(key);
+	});
 
-		var fighterPortrait = $("<img>");
-		fighterPortrait.attr("src", imagesPath + fighters[fightersArray[i]].image);
-		fighterPortrait.addClass("img-fluid text-center");
+	//function to populate the characters' row
+	var populateFightersRow = function(row, numFrom, numTo) {
+		for (var i = numFrom; i < numTo; i++) {	
+			var fighterContainer = $("<div>");		
+			fighterContainer.addClass("col-1 fighterSelect");
+			fighterContainer.attr("value", fighters[fightersArray[i]].value);
 
-		fighterContainer.append(fighterPortrait);
-		$(row).append(fighterContainer);
-	}
-}
+			var fighterPortrait = $("<img>");
+			fighterPortrait.attr("src", imagesPath + fighters[fightersArray[i]].image);
+			fighterPortrait.addClass("img-fluid text-center");
 
-populateFightersRow("#firstRow", 0, 5);
-populateFightersRow("#secondRow", 5, 10);
-
-var fighterStats = function (obj, element) {
-	var health = $("<p>").text("Health: " + obj.health);
-	var attack = $("<p>").text("Attack: " + obj.attack);
-	var counterAttack = $("<p>").text("Counter Attack: " + obj.counterAttack);
-
-	$(element).append(health);
-	$(element).append(attack);
-	$(element).append(counterAttack);
-};
-
-var fighterSlide = function(obj, fighter) {
-
-	if ( $("#" + fighter + " img").length ) {
-		$("#" + fighter + " img").remove();
-		$("." + fighter + "Stats").empty();
+			fighterContainer.append(fighterPortrait);
+			$(row).append(fighterContainer);
+		}
 	}
 
-	var fighterImage = $("<img>");
-	fighterImage.addClass("img-fluid");
-	fighterImage.attr("src", imagesPath + obj.image);
-	fighterImage.attr("alt", obj.name);
+	populateFightersRow("#firstRow", 0, 5);
+	populateFightersRow("#secondRow", 5, 10);
 
-	$("#"+fighter + " .fighterNickname").before(fighterImage);
+	//function to update the fighters stats (HP, Attack, Counter-Attack)
+	var fighterStats = function (obj, element) {
+		var health = $("<p>").text("Health: " + obj.health);
+		var attack = $("<p>").text("Attack: " + obj.attack);
+		var counterAttack = $("<p>").text("Counter Attack: " + obj.counterAttack);
 
-	$("#"+fighter + " .fighterNickname").text(obj.nickname);
+		$(element).append(health);
+		$(element).append(attack);
+		$(element).append(counterAttack);
+	};
 
-	fighterStats(obj, "."+fighter+"Stats");
-}
+	//function to create fighter's portrait and slide animation
+	var fighterSlide = function(obj, fighter) {
 
-var onHover = function () {
-	if (!isPlayerSelected && !fighters[$(this).attr("value")].selected) {
-		
-		player = fighters[$(this).attr("value")];
+		if ( $("#" + fighter + " img").length ) {
+			$("#" + fighter + " img").remove();
+			$("." + fighter + "Stats").empty();
+		}
 
-		$(this).addClass("playerHover");
+		var fighterImage = $("<img>");
+		fighterImage.addClass("img-fluid");
+		fighterImage.attr("src", imagesPath + obj.image);
+		fighterImage.attr("alt", obj.name);
 
-		fighterSlide(player, "player");
+		$("#"+fighter + " .fighterNickname").before(fighterImage);
 
-	} else if (!isChallengerSelected && !fighters[$(this).attr("value")].selected) {
+		$("#"+fighter + " .fighterNickname").text(obj.nickname);
 
-		challenger = fighters[$(this).attr("value")];
-
-		$(this).addClass("challengerHover");
-
-		fighterSlide(challenger, "challenger");
-	}
-};
-
-var outHover = function () {
-	if (!isPlayerSelected) {
-		$(this).removeClass("playerHover");
-	} else if (!isChallengerSelected) {
-		$(this).removeClass("challengerHover");
-	}
-};
-
-$(".fighterSelect").hover( onHover, outHover );
-
-$(".fighterSelect").on("click", function () {
-	if (!isPlayerSelected && !isChallengerSelected && !fighters[$(this).attr("value")].selected) {
-		player = fighters[$(this).attr("value")];
-		defaultAttackPoint = player.attack;
-		player.selected = true;
-		isPlayerSelected = true;		
-		$(this).addClass("playerHover noCursor");
-	}
-	else if (isPlayerSelected && !isChallengerSelected && !fighters[$(this).attr("value")].selected) {
-		challenger = fighters[$(this).attr("value")];
-		challenger.selected = true;
-		isChallengerSelected = true;
-		$("#attack").removeAttr("disabled","disabled");
-		$(this).addClass("challengerHover noCursor");
-		$(".characters").addClass("noCursor");
-		$("#attack").removeAttr("disabled","disabled");
-		$("#result").empty();
-	} 
-
-	else if (!isChallengerSelected && player === fighters[$(this).attr("value")] && !player.active) {
-		player.selected = false;
-		isPlayerSelected = false;
-		$(this).removeClass("playerHover noCursor");
+		fighterStats(obj, "."+fighter+"Stats");
 	}
 
-	else if (isPlayerSelected && isChallengerSelected && challenger === fighters[$(this).attr("value")] && !challenger.active) {
-		challenger.selected = false;
-		isChallengerSelected = false;
-		$(this).removeClass("playerHover noCursor");
+	//function for mouseIn fighter
+	var onHover = function () {
+		if (!isPlayerSelected && !fighters[$(this).attr("value")].selected) {
+			
+			player = fighters[$(this).attr("value")];
+
+			$(this).addClass("playerHover");
+
+			fighterSlide(player, "player");
+
+		} else if (!isChallengerSelected && !fighters[$(this).attr("value")].selected) {
+
+			challenger = fighters[$(this).attr("value")];
+
+			$(this).addClass("challengerHover");
+
+			fighterSlide(challenger, "challenger");
+		}
+	};
+
+	//function for mouseOut fighter
+	var outHover = function () {
+		if (!isPlayerSelected) {
+			$(this).removeClass("playerHover");
+		} else if (!isChallengerSelected) {
+			$(this).removeClass("challengerHover");
+		}
+	};
+
+	//function to hover state
+	$(".fighterSelect").hover( onHover, outHover );
+
+	//different scenarios for character selection
+	$(".fighterSelect").on("click", function () {
+		if (!isPlayerSelected && !isChallengerSelected && !fighters[$(this).attr("value")].selected) {
+			player = fighters[$(this).attr("value")];
+			defaultAttackPoint = player.attack;
+			player.selected = true;
+			isPlayerSelected = true;		
+			$(this).addClass("playerHover noCursor");
+		}
+		else if (isPlayerSelected && !isChallengerSelected && !fighters[$(this).attr("value")].selected) {
+			challenger = fighters[$(this).attr("value")];
+			challenger.selected = true;
+			isChallengerSelected = true;
+			$("#attack").removeAttr("disabled","disabled");
+			$(this).addClass("challengerHover noCursor");
+			$(".characters").addClass("noCursor");
+			$("#attack").removeAttr("disabled","disabled");
+			$("#result").empty();
+		}
+		else if (!isChallengerSelected && player === fighters[$(this).attr("value")] && !player.active) {
+			player.selected = false;
+			isPlayerSelected = false;
+			$(this).removeClass("playerHover noCursor");
+		}
+		else if (isPlayerSelected && isChallengerSelected && challenger === fighters[$(this).attr("value")] && !challenger.active) {
+			challenger.selected = false;
+			isChallengerSelected = false;
+			$(this).removeClass("playerHover noCursor");
+			$(".characters").removeClass("noCursor");
+			$("#attack").attr("disabled","disabled");
+		}
+	});
+
+	//function used when character is defeated 
+	var updateFightersInfo = function(obj, fighter) {
+		$("div[value=" + obj.value + "]").addClass("defeated");
+		$("div[value=" + obj.value + "]").removeClass(fighter+"Hover");
 		$(".characters").removeClass("noCursor");
+		obj.defeated = true;
 		$("#attack").attr("disabled","disabled");
 	}
-});
 
-var updateFightersInfo = function(obj, fighter) {
-	$("div[value=" + obj.value + "]").addClass("defeated");
-	$("div[value=" + obj.value + "]").removeClass(fighter+"Hover");
-	$(".characters").removeClass("noCursor");
-	obj.defeated = true;
-	$("#attack").attr("disabled","disabled");
-}
+	//for attack button
+	$("#attack").on("click", function() {
 
-$("#attack").on("click", function() {
+		$(".playerStats").empty();
+		$(".challengerStats").empty();
 
-	$(".playerStats").empty();
-	$(".challengerStats").empty();
+		player.active = true;
+		challenger.active = true;
 
-	player.active = true;
-	challenger.active = true;
+		challenger.health -= player.attack;
+		player.attack += defaultAttackPoint;
 
-	challenger.health -= player.attack;
-	player.attack += defaultAttackPoint;
+		player.health -= challenger.counterAttack;
 
-	player.health -= challenger.counterAttack;
-
-	fighterStats(player, ".playerStats");
-	fighterStats(challenger, ".challengerStats");
+		fighterStats(player, ".playerStats");
+		fighterStats(challenger, ".challengerStats");
 
 
-	if (player.health <= 0 && challenger.health <= 0) {
-		updateFightersInfo(player,"player");
-		updateFightersInfo(challenger,"challenger");
-		$("#result").html("You <span>L</span>ose");
-	}
-	else if (player.health <= 0) {
-		updateFightersInfo(player,"player");
-		$("#result").html("You <span>L</span>ose");
-	}
-	else if (challenger.health <= 0) {
-		updateFightersInfo(challenger,"challenger");
-		isChallengerSelected = false;
-		$("#result").html("You <span>W</span>in");
-	}
-});
+		if (player.health <= 0 && challenger.health <= 0) {
+			updateFightersInfo(player,"player");
+			updateFightersInfo(challenger,"challenger");
+			$("#result").html("You <span>L</span>ose");
+		}
+		else if (player.health <= 0) {
+			updateFightersInfo(player,"player");
+			$("#result").html("You <span>L</span>ose");
+		}
+		else if (challenger.health <= 0) {
+			updateFightersInfo(challenger,"challenger");
+			isChallengerSelected = false;
+			$("#result").html("You <span>W</span>in");
+		}
+	});
 
-$('#reset').click(function() {
-	location.reload();
-});
+	//resets the game
+	$('#reset').click(function() {
+		location.reload();
+	});
 
 });
