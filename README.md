a93bb4dff0d747f1ccc30a76bcb11e36f7d1e825
Sandro Wong <saloiofun@gmail.com>
2017-12-25 20:39:47 -0800

    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# week-4-game
-RPG Game
+# RPG-Game
+
+![Demo](assets/images/demo.png)
+
+This RPG Game is inspired on Street Fighter's Arcade game
+
+Link: https://saloiofun.github.io/RPG-game/
+
+## Getting Started
+
+* Choose a character by clicking on the fighter's picture. You will fight as that character for the rest of the game.
+* Choose an opponent by clicking on an enemy's picture.
+* Defeat all of the remaining fighters.
+* Click on "Attack" button to attack your enemy.
+    * Whenever the player clicks attack, their character damages the defender. The opponent will lose HP (health points). These points are displayed at the bottom of the defender's picture.
+    * The opponent character will instantly counter the attack. When that happens, the player's character will lose some of their HP. These points are shown at the bottom of the player character's picture.
+* Keep hitting the attack button in an effort to defeat their opponent.
+* When the opponent's HP is reduced to zero or below, the picture will be grayed out. You can now choose a new opponent.
+* You win the game by defeating all enemy characters. You lose the game the game if your character's HP falls to zero or below.
+
+## Notes
+
+* Each character in the game has 3 attributes: Health Points, Attack Power and Counter Attack Power.
+
+* Each time the player attacks, their character's Attack Power increases by its base Attack Power.
+
+  For example, if the base Attack Power is 6, each attack will increase the Attack Power by 6 (12, 18, 24, 30 and so on).
+
+* The enemy character only has Counter Attack Power.
+
+* Unlike the player's Attack Points, Counter Attack Power never changes.
+
+* No characters in the game can heal or recover Health Points.
+
+A winning player must pick their characters wisely by first fighting an enemy with low Counter Attack Power. This will allow them to grind Attack Power and to take on enemies before they lose all of their Health Points. Healing options would mess with this dynamic.
+Your players should be able to win and lose the game no matter what character they choose. The challenge should come from picking the right enemies, not choosing the strongest player.
+
+## Tools used
+
+* HTML 5
+* CSS
+* JavaScript
+* Jquery
+* Bootstrap 4
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,312 +1,302 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  // Street Fighter Characters
+  var fighters = {
 
-	// Street Fighter Characters
-	var fighters = {
+    ryu: {
+      nickname: 'Ryu',
+      value: 'ryu',
+      health: 1000,
+      attack: 30,
+      counterAttack: 45,
+      selected: false,
+      active: false,
+      image: 'ryu.png',
+      sound: 'ryu.wav'
+    },
 
-		ryu: {
-			nickname: "Ryu",
-			value: "ryu",
-			health: 1000,
-			attack: 30,
-			counterAttack: 45,
-			selected: false,
-			active: false,
-			image: "ryu.png",
-			sound: "ryu.wav"
-		},
+    ken: {
+      nickname: 'Ken',
+      value: 'ken',
+      health: 1000,
+      attack: 25,
+      counterAttack: 45,
+      selected: false,
+      active: false,
+      image: 'ken.png',
+      sound: 'ken.wav'
+    },
 
-		ken: {
-			nickname: "Ken",
-			value: "ken",
-			health: 1000,
-			attack: 25,
-			counterAttack: 45,
-			selected: false,
-			active: false,
-			image: "ken.png",
-			sound: "ken.wav"
-		},
+    akuma: {
+      nickname: 'Akuma',
+      value: 'akuma',
+      health: 950,
+      attack: 30,
+      counterAttack: 45,
+      selected: false,
+      active: false,
+      image: 'akuma.png',
+      sound: 'akuma.wav'
+    },
 
-		akuma: {
-			nickname: "Akuma",
-			value: "akuma",
-			health: 950,
-			attack: 30,
-			counterAttack: 45,
-			selected: false,
-			active: false,
-			image: "akuma.png",
-			sound: "akuma.wav"
-		},
+    dan: {
+      nickname: 'Dan',
+      value: 'dan',
+      health: 1200,
+      attack: 20,
+      counterAttack: 35,
+      selected: false,
+      active: false,
+      image: 'dan.png',
+      sound: 'dan.wav'
+    },
 
-		dan: {
-			nickname: "Dan",
-			value: "dan",
-			health: 1200,
-			attack: 20,
-			counterAttack: 35,
-			selected: false,
-			active: false,
-			image: "dan.png",
-			sound: "dan.wav"
-		},
+    dhalsim: {
+      nickname: 'Dhalsim',
+      value: 'dhalsim',
+      health: 1100,
+      attack: 25,
+      counterAttack: 40,
+      selected: false,
+      active: false,
+      image: 'dhalsim.png',
+      sound: 'dhalsim.wav'
+    },
 
-		dhalsim: {
-			nickname: "Dhalsim",
-			value: "dhalsim",
-			health: 1100,
-			attack: 25,
-			counterAttack: 40,
-			selected: false,
-			active: false,
-			image: "dhalsim.png",
-			sound: "dhalsim.wav"
-		},
+    chunLi: {
+      nickname: 'Chun-Li',
+      value: 'chunLi',
+      health: 950,
+      attack: 35,
+      counterAttack: 35,
+      selected: false,
+      active: false,
+      image: 'chunLi.png',
+      sound: 'chunLi.wav'
+    },
 
-		chunLi: {
-			nickname: "Chun-Li",
-			value: "chunLi",
-			health: 950,
-			attack: 35,
-			counterAttack: 35,
-			selected: false,
-			active: false,
-			image: "chunLi.png",
-			sound: "chunLi.wav"
-		},
+    sakura: {
+      nickname: 'Sakura',
+      value: 'sakura',
+      health: 950,
+      attack: 35,
+      counterAttack: 30,
+      selected: false,
+      active: false,
+      image: 'sakura.png',
+      sound: 'sakura.wav'
+    },
 
-		sakura: {
-			nickname: "Sakura",
-			value: "sakura",
-			health: 950,
-			attack: 35,
-			counterAttack: 30,
-			selected: false,
-			active: false,
-			image: "sakura.png",
-			sound: "sakura.wav"
-		},
+    guile: {
+      nickname: 'Guile',
+      value: 'guile',
+      health: 950,
+      attack: 30,
+      counterAttack: 40,
+      selected: false,
+      active: false,
+      image: 'guile.png',
+      sound: 'guile.wav'
+    },
 
-		guile: {
-			nickname: "Guile",
-			value: "guile",
-			health: 950,
-			attack: 30,
-			counterAttack: 40,
-			selected: false,
-			active: false,
-			image: "guile.png",
-			sound: "guile.wav"
-		},
+    elFuerte: {
+      nickname: 'El Fuerte',
+      value: 'elFuerte',
+      health: 900,
+      attack: 30,
+      counterAttack: 35,
+      selected: false,
+      active: false,
+      image: 'elFuerte.png',
+      sound: 'elFuerte.wav'
+    },
 
-		elFuerte: {
-			nickname: "El Fuerte",
-			value: "elFuerte",
-			health: 900,
-			attack: 30,
-			counterAttack: 35,
-			selected: false,
-			active: false,
-			image: "elFuerte.png",
-			sound: "elFuerte.wav"
-		},
+    feiLong: {
+      nickname: 'Fei Long',
+      value: 'feiLong',
+      health: 950,
+      attack: 30,
+      counterAttack: 40,
+      selected: false,
+      active: false,
+      image: 'feiLong.png',
+      sound: 'feiLong.wav'
+    }
+  }
 
-		feiLong: {
-			nickname: "Fei Long",
-			value: "feiLong",
-			health: 950,
-			attack: 30,
-			counterAttack: 40,
-			selected: false,
-			active: false,
-			image: "feiLong.png",
-			sound: "feiLong.wav"
-		}
-	};
+  // declare variables
+  var fightersArray = []
+  var imagesPath = 'assets/images/'
+  var soundPath = 'assets/sound/'
+  var player
+  var challenger
+  var defaultAttackPoint
+  var isPlayerSelected = false
+  var isChallengerSelected = false
 
-	//declare variables
-	var fightersArray = [];
-	var imagesPath = "assets/images/";
-	var soundPath = "assets/sound/";
-	var player;
-	var challenger;
-	var defaultAttackPoint;
-	var isPlayerSelected = false;
-	var isChallengerSelected = false;
+  $('#attack').attr('disabled', 'disabled')
 
-	$("#attack").attr("disabled","disabled");
+  // push fighters objects into fightersArray
+  $.each(fighters, function (key) {
+    fightersArray.push(key)
+  })
 
-	//push fighters objects into fightersArray
-	$.each(fighters, function(key) {
-		fightersArray.push(key);
-	});
+  // function to populate the characters' row
+  var populateFightersRow = function (row, numFrom, numTo) {
+    for (var i = numFrom; i < numTo; i++) {
+      var fighterContainer = $('<div>')
+      fighterContainer.addClass('col-1 fighterSelect')
+      fighterContainer.attr('value', fighters[fightersArray[i]].value)
 
-	//function to populate the characters' row
-	var populateFightersRow = function(row, numFrom, numTo) {
-		for (var i = numFrom; i < numTo; i++) {	
-			var fighterContainer = $("<div>");		
-			fighterContainer.addClass("col-1 fighterSelect");
-			fighterContainer.attr("value", fighters[fightersArray[i]].value);
+      var fighterPortrait = $('<img>')
+      fighterPortrait.attr('src', imagesPath + fighters[fightersArray[i]].image)
+      fighterPortrait.addClass('img-fluid text-center')
 
-			var fighterPortrait = $("<img>");
-			fighterPortrait.attr("src", imagesPath + fighters[fightersArray[i]].image);
-			fighterPortrait.addClass("img-fluid text-center");
+      fighterContainer.append(fighterPortrait)
+      $(row).append(fighterContainer)
+    }
+  }
 
-			fighterContainer.append(fighterPortrait);
-			$(row).append(fighterContainer);
-		}
-	}
+  populateFightersRow('#firstRow', 0, 5)
+  populateFightersRow('#secondRow', 5, 10)
 
-	populateFightersRow("#firstRow", 0, 5);
-	populateFightersRow("#secondRow", 5, 10);
+	// function to update the fighters stats (HP, Attack, Counter-Attack)
+  var fighterStats = function (obj, element) {
+    var health = $('<p>').text('Health: ' + obj.health)
+    var attack = $('<p>').text('Attack: ' + obj.attack)
+    var counterAttack = $('<p>').text('Counter Attack: ' + obj.counterAttack)
 
-	//function to update the fighters stats (HP, Attack, Counter-Attack)
-	var fighterStats = function (obj, element) {
-		var health = $("<p>").text("Health: " + obj.health);
-		var attack = $("<p>").text("Attack: " + obj.attack);
-		var counterAttack = $("<p>").text("Counter Attack: " + obj.counterAttack);
+    $(element).append(health)
+    $(element).append(attack)
+    $(element).append(counterAttack)
+  }
 
-		$(element).append(health);
-		$(element).append(attack);
-		$(element).append(counterAttack);
-	};
+  // function to create fighter's portrait and slide animation
+  var fighterSlide = function (obj, fighter) {
+    if ($('#' + fighter + ' img').length) {
+      $('#' + fighter + ' img').remove()
+      $('.' + fighter + 'Stats').empty()
+    }
 
-	//function to create fighter's portrait and slide animation
-	var fighterSlide = function(obj, fighter) {
+    var fighterImage = $('<img>')
+    fighterImage.addClass('img-fluid')
+    fighterImage.attr('src', imagesPath + obj.image)
+    fighterImage.attr('alt', obj.nickname)
 
-		if ( $("#" + fighter + " img").length ) {
-			$("#" + fighter + " img").remove();
-			$("." + fighter + "Stats").empty();
-		}
+    $('#' + fighter + ' .fighterNickname').before(fighterImage)
+    $('#' + fighter + ' .fighterNickname').text(obj.nickname)
+    fighterStats(obj, '.' + fighter + 'Stats')
+  }
 
-		var fighterImage = $("<img>");
-		fighterImage.addClass("img-fluid");
-		fighterImage.attr("src", imagesPath + obj.image);
-		fighterImage.attr("alt", obj.nickname);
+  // function for mouseIn fighter
+  var onHover = function () {
+    if (!isPlayerSelected && !fighters[$(this).attr('value')].selected) {
+      player = fighters[$(this).attr('value')]
+      $(this).addClass('playerHover')
+      fighterSlide(player, 'player')
+    } else if (!isChallengerSelected && !fighters[$(this).attr('value')].selected) {
+      challenger = fighters[$(this).attr('value')]
+      $(this).addClass('challengerHover')
+      fighterSlide(challenger, 'challenger')
+    }
+  }
 
-		$("#"+fighter + " .fighterNickname").before(fighterImage);
-		$("#"+fighter + " .fighterNickname").text(obj.nickname);
-		fighterStats(obj, "."+fighter+"Stats");
-	}
+  // function for mouseOut fighter
+  var outHover = function () {
+    if (!isPlayerSelected) {
+      $(this).removeClass('playerHover')
+    } else if (!isChallengerSelected) {
+      $(this).removeClass('challengerHover')
+    }
+  }
 
-	//function for mouseIn fighter
-	var onHover = function () {
-		if (!isPlayerSelected && !fighters[$(this).attr("value")].selected) {			
-			player = fighters[$(this).attr("value")];
-			$(this).addClass("playerHover");
-			fighterSlide(player, "player");
+  // function to hover state
+  $('.fighterSelect').hover(onHover, outHover)
 
-		} else if (!isChallengerSelected && !fighters[$(this).attr("value")].selected) {
-			challenger = fighters[$(this).attr("value")];
-			$(this).addClass("challengerHover");
-			fighterSlide(challenger, "challenger");
-		}
-	};
+  // different scenarios for character selection
+  $('.fighterSelect').on('click', function () {
+    if (!isPlayerSelected && !isChallengerSelected && !fighters[$(this).attr('value')].selected) {
+      player = fighters[$(this).attr('value')]
+      defaultAttackPoint = player.attack
+      player.selected = true
+      isPlayerSelected = true
+      $(this).addClass('playerHover noCursor')
+      var audio = new Audio(soundPath + player.sound)
+      audio.play()
+    } else if (isPlayerSelected && !isChallengerSelected && !fighters[$(this).attr('value')].selected) {
+      challenger = fighters[$(this).attr('value')]
+      challenger.selected = true
+      isChallengerSelected = true
+      $(this).addClass('challengerHover noCursor')
+      $('.characters').addClass('noCursor')
+      $('#result').empty()
+      $('#attack').removeAttr('disabled', 'disabled')
+      var audio = new Audio(soundPath + challenger.sound)
+      audio.play()
+    } else if (!isChallengerSelected && player === fighters[$(this).attr('value')] && !player.active) {
+      player.selected = false
+      isPlayerSelected = false
+      $(this).removeClass('playerHover noCursor')
+    } else if (isPlayerSelected && isChallengerSelected && challenger === fighters[$(this).attr('value')] && !challenger.active) {
+      challenger.selected = false
+      isChallengerSelected = false
+      $(this).removeClass('playerHover noCursor')
+      $('.characters').removeClass('noCursor')
+      $('#attack').attr('disabled', 'disabled')
+    }
+  })
 
-	//function for mouseOut fighter
-	var outHover = function () {
-		if (!isPlayerSelected) {
-			$(this).removeClass("playerHover");
-		} else if (!isChallengerSelected) {
-			$(this).removeClass("challengerHover");
-		}
-	};
+  // function used when character is defeated
+  var updateFightersInfo = function (obj, fighter) {
+    $('div[value=' + obj.value + ']').addClass('defeated')
+    $('div[value=' + obj.value + ']').removeClass(fighter + 'Hover')
+    $('.characters').removeClass('noCursor')
+    obj.defeated = true
+    $('#attack').attr('disabled', 'disabled')
+  }
 
-	//function to hover state
-	$(".fighterSelect").hover( onHover, outHover );
+  // for attack button
+  $('#attack').on('click', function () {
+    $('.playerStats').empty()
+    $('.challengerStats').empty()
 
-	//different scenarios for character selection
-	$(".fighterSelect").on("click", function () {
-		if (!isPlayerSelected && !isChallengerSelected && !fighters[$(this).attr("value")].selected) {
-			player = fighters[$(this).attr("value")];
-			defaultAttackPoint = player.attack;
-			player.selected = true;
-			isPlayerSelected = true;		
-			$(this).addClass("playerHover noCursor");
-			var audio = new Audio(soundPath + player.sound);
-			audio.play();
-		}
-		else if (isPlayerSelected && !isChallengerSelected && !fighters[$(this).attr("value")].selected) {
-			challenger = fighters[$(this).attr("value")];
-			challenger.selected = true;
-			isChallengerSelected = true;
-			$(this).addClass("challengerHover noCursor");
-			$(".characters").addClass("noCursor");
-			$("#result").empty();
-			$("#attack").removeAttr("disabled","disabled");
-			var audio = new Audio(soundPath + challenger.sound);
-			audio.play();
-		}
-		else if (!isChallengerSelected && player === fighters[$(this).attr("value")] && !player.active) {
-			player.selected = false;
-			isPlayerSelected = false;
-			$(this).removeClass("playerHover noCursor");
-		}
-		else if (isPlayerSelected && isChallengerSelected && challenger === fighters[$(this).attr("value")] && !challenger.active) {
-			challenger.selected = false;
-			isChallengerSelected = false;
-			$(this).removeClass("playerHover noCursor");
-			$(".characters").removeClass("noCursor");
-			$("#attack").attr("disabled","disabled");
-		}
-	});
+    player.active = true
+    challenger.active = true
 
-	//function used when character is defeated 
-	var updateFightersInfo = function(obj, fighter) {
-		$("div[value=" + obj.value + "]").addClass("defeated");
-		$("div[value=" + obj.value + "]").removeClass(fighter+"Hover");
-		$(".characters").removeClass("noCursor");
-		obj.defeated = true;
-		$("#attack").attr("disabled","disabled");
-	}
+    challenger.health -= player.attack
+    player.attack += defaultAttackPoint
+    player.health -= challenger.counterAttack
 
-	//for attack button
-	$("#attack").on("click", function() {
+    fighterStats(player, '.playerStats')
+    fighterStats(challenger, '.challengerStats')
 
-		$(".playerStats").empty();
-		$(".challengerStats").empty();
+    if (player.health > 0 && challenger.health > 0) {
+      $('#result').addClass('normal-text')
+      var fightStatus = '<p>You attacked ' + challenger.nickname + ' for ' + player.attack + ' damage.</p>' +
+      '<p>' + challenger.nickname + ' attacked you back for ' + challenger.counterAttack + ' damage.</p>'
+      $('#result').html(fightStatus)
+    }
 
-		player.active = true;
-		challenger.active = true;
+    if (player.health <= 0 && challenger.health <= 0) {
+      $('#result').removeClass('normal-text')
+      updateFightersInfo(player, 'player')
+      updateFightersInfo(challenger, 'challenger')
+      $('#result').html('<h1>You <span>L</span>ose</h1>')
+      $('.characters').addClass('noCursor')
+    } else if (player.health <= 0) {
+      $('#result').removeClass('normal-text')
+      updateFightersInfo(player, 'player')
+      $('#result').html('<h1>You <span>L</span>ose</h1>')
+      $('.characters').addClass('noCursor')
+    } else if (challenger.health <= 0) {
+      $('#result').removeClass('normal-text')
+      updateFightersInfo(challenger, 'challenger')
+      isChallengerSelected = false
+      $('#result').html('<h1>You <span>W</span>in</h1>')
+    }
+  })
 
-		challenger.health -= player.attack;
-		player.attack += defaultAttackPoint;
-		player.health -= challenger.counterAttack;
-
-		fighterStats(player, ".playerStats");
-		fighterStats(challenger, ".challengerStats");
-
-		if (player.health > 0 && challenger.health > 0) {
-			$("#result").addClass("normal-text");
-			var fightStatus = "<p>You attacked " + challenger.nickname + " for " + player.attack + " damage.</p>"
-			+ "<p>" + challenger.nickname + " attacked you back for " + challenger.counterAttack + " damage.</p>";
-			$("#result").html(fightStatus);
-		}
-
-		if (player.health <= 0 && challenger.health <= 0) {
-			$("#result").removeClass("normal-text");
-			updateFightersInfo(player,"player");
-			updateFightersInfo(challenger,"challenger");
-			$("#result").html("<h1>You <span>L</span>ose</h1>");
-			$(".characters").addClass("noCursor");
-		}
-		else if (player.health <= 0) {
-			$("#result").removeClass("normal-text");
-			updateFightersInfo(player,"player");
-			$("#result").html("<h1>You <span>L</span>ose</h1>");
-			$(".characters").addClass("noCursor");
-		}
-		else if (challenger.health <= 0) {
-			$("#result").removeClass("normal-text");
-			updateFightersInfo(challenger,"challenger");
-			isChallengerSelected = false;
-			$("#result").html("<h1>You <span>W</span>in</h1>");			
-		}
-	});
-
-	//resets the game
-	$('#reset').click(function() {
-		location.reload();
-	});
-
-});
+  // resets the game
+  $('#reset').click(function () {
+    location.reload()
+  })
+})
